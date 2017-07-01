f44c6c518aa9ebfcdb306ec147c9d80aee37c60a
Zenhobby <877756324@qq.com>
2017-07-01 23:48:40 +0800

    Day 8 Update

---
+++ www/config_override.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.
+'''
+
+__author__ = 'Michael Liao'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'db': 'awesome'
+    }
+}
